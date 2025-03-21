46c10832b35f1b857712312ee7b821a00c8a5ccc
linus <linus.f@protonmail.ch>
2025-03-21 16:29:27 +0100

    Added flake

--- flake.nix
+++ flake.nix
@@ -1,9 +1,4 @@
 {
   description = "Flake for project outputs.";
-  outputs = { ... }: {
-    templates.wp-flake = {
-      path = ./templates/wp-project;
-      description = "A wordpress template using Nix flake and Devenv.";
-    };
-  };
+  outputs = { ... }: {};
 }
