64be9eac0e6a5b89b3e9f6ddce729716b3ca0a93
linus <linus.f@protonmail.ch>
2025-10-28 19:52:14 +0100

    Added convenience script to get LAN host

--- lib/devenv/common.nix
+++ lib/devenv/common.nix
@@ -8,4 +8,18 @@
   processes.current-env.exec = ''
     devenv info
   '';
+
+  scripts = {
+    get-network-host = {
+      exec = ''
+        ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p'
+
+        if [[ $? -ne 0 ]]; then
+          echo "Failed to get development network host."
+          exit 1
+        fi
+      '';
+      packages = with pkgs; [ gnused ];
+    };
+  };
 }
